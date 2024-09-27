02d787ef5dd9f585aad838cd9d4abc5e4e40e46e
Ridwan <ridwanyinusa15@gmail.com>
2024-09-27 16:37:30 +0100

    chore: add tailwind config and refactor header section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./styles/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
